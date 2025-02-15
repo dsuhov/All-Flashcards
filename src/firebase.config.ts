4fd589ebde412be337610b841951610b93b1e914
Dmitiy Sukhov <dstujev@gmail.com>
2025-02-15 13:30:11 +0300

    firebase tools setup

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,19 +1,10 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-// import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
 
 export const app = initializeApp(firebaseConfig);
 
-// const db = getFirestore(app);
-// Get a list of cities from your database
-// async function getCities(db) {
-//   const citiesCol = collection(db, 'cities');
-//   const citySnapshot = await getDocs(citiesCol);
-//   const cityList = citySnapshot.docs.map(doc => doc.data());
-//   return cityList;
-// }
 export const firebaseAuth = getAuth();
 export const db = getFirestore(app);
